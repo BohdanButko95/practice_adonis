0b52ddec638eda6bfc7373d28c91191e3f3db733
Bohdan Butko <bohdanbutko@gmail.com>
2019-01-10 21:18:18 +0200

    Add routes for product attributes

--- start/routes.js
+++ start/routes.js
@@ -3,7 +3,9 @@ const Route = use('Route');
 Route.group(() => {
   Route.post('login', 'UserController.login');
 
-  Route.resource('products/types', 'ProductTypeController').apiOnly();
   Route.resource('attributes', 'AttributeController').apiOnly();
+  Route.resource('products/types', 'ProductTypeController').apiOnly();
+  Route.resource('products/types/:id/attributes', 'ProductTypeAttributeController').apiOnly();
   Route.resource('products', 'ProductController').apiOnly();
+  Route.resource('products/:id/attributes', 'ProductAttributeController').apiOnly();
 }).prefix('api/v1');
