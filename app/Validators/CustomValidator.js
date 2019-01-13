4bb7107d899724cf0ea4f9212acb6bdfdec8f785
Bohdan Butko <bohdanbutko@gmail.com>
2019-01-13 19:25:09 +0200

    Extend validator with 'exists' rule

--- app/Validators/CustomValidator.js
+++ app/Validators/CustomValidator.js
@@ -1,3 +1,25 @@
+const Validator = use('Validator');
+const Database = use('Database');
+
+const existsFn = async (data, field, message, args, get) => {
+  const value = get(data, field);
+
+  if (!value) {
+    return;
+  }
+
+  const [table, column] = args;
+  const row = await Database.table(table)
+    .where(column, value)
+    .first();
+
+  if (!row) {
+    throw message;
+  }
+};
+
+Validator.extend('exists', existsFn);
+
 class CustomValidator {
   async fails(errorMessages) {
     return this.ctx.response.json(errorMessages);
