a4d8b42f4f1b4fd0d557032637788df4af714044
Bohdan Butko <bohdanbutko@gmail.com>
2019-01-13 18:28:36 +0200

    Add getEntities() method to product repository
    Method provides filtering and sorting for products

--- app/Models/Repositories/Product.js
+++ app/Models/Repositories/Product.js
@@ -1,6 +1,35 @@
 const Prod = use('App/Models/Product');
+const Database = use('Database');
 
 class Product {
+  static async getEntities({ type_id: typeId, name, owner_id: ownerId, price, created_at: createdAt }) {
+    const filterByType = typeId || null;
+    const filterByName = name || null;
+    const filterByOwner = ownerId || null;
+    const sortByPrice = ['asc', 'desc'].includes(price) ? price : null;
+    const sortByDate = ['asc', 'desc'].includes(createdAt) ? createdAt : null;
+
+    const query = Database.table('products');
+
+    if (filterByType) {
+      query.where('type_id', Number(filterByType));
+    }
+    if (filterByName) {
+      query.where('name', 'LIKE', `%${filterByName}%`);
+    }
+    if (filterByOwner) {
+      query.where('user_id', Number(filterByOwner));
+    }
+    if (sortByPrice) {
+      query.orderBy('price', sortByPrice);
+    }
+    if (sortByDate) {
+      query.orderBy('created_at', sortByDate);
+    }
+
+    return query;
+  }
+
   static async storeEntity({ name, price, type_id: typeId, user_id: userId }) {
     const product = new Prod();
     product.name = name;
