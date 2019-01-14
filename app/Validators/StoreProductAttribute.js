4ce5d2c774c873f356c20b838bb0f3d67f70b723
Bohdan Butko <bohdanbutko@gmail.com>
2019-01-14 16:06:01 +0200

    Change rules for StoreProductAttribute and StoreProductTypeAttribute validators

--- app/Validators/StoreProductAttribute.js
+++ app/Validators/StoreProductAttribute.js
@@ -4,7 +4,6 @@ class StoreProductAttribute extends CustomValidator {
   get rules() {
     return {
       value: 'required',
-      product_id: 'required|exists:products,id',
       attribute_id: 'required|exists:attributes,id'
     };
   }
@@ -12,8 +11,6 @@ class StoreProductAttribute extends CustomValidator {
   get messages() {
     return {
       'value.required': 'You must provide a value',
-      'product_id.required': 'You must provide a product_id',
-      'product_id.exists': "Provided product_id doesn't exists",
       'attribute_id.required': 'You must provide an attribute_id',
       'attribute_id.exists': "Provided attribute_id doesn't exists"
     };
--- app/Validators/StoreProductTypeAttribute.js
+++ app/Validators/StoreProductTypeAttribute.js
@@ -3,16 +3,13 @@ const CustomValidator = use('App/Validators/CustomValidator');
 class StoreProductTypeAttribute extends CustomValidator {
   get rules() {
     return {
-      product_type_id: 'required|exists:product_types,id',
-      attribute_id: 'required|exists:attributes,id'
+      attribute_id: 'required|exists:attributes,id|test'
     };
   }
 
   get messages() {
     return {
-      'product_type_id.required': 'You must provide product_type_id',
-      'product_type_id.exists': "Provided product_type_id doesn't exists",
-      'attribute_id.required': 'You must provide attribute_id',
+      'attribute_id.required': 'You must provide an attribute_id',
       'attribute_id.exists': "Provided attribute_id doesn't exists"
     };
   }
