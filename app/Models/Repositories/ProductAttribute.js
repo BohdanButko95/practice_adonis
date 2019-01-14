75576383120ab0542e0b4e0b39d6da8e2ff4b392
Bohdan Butko <bohdanbutko@gmail.com>
2019-01-14 15:31:46 +0200

    Rewrite repository methods for ProductAttribute and ProductTypeAttribute

--- app/Models/Repositories/ProductAttribute.js
+++ app/Models/Repositories/ProductAttribute.js
@@ -3,42 +3,42 @@ const Product = use('App/Models/Product');
 const ProductTypeAttribute = use('App/Models/ProductTypeAttribute');
 
 class ProductAttribute {
-  static async storeEntity({ value, product_id: productId, attribute_id: attributeId }) {
-    const product = await Product.findOrFail(productId);
-    const type = await product.type().fetch();
+  static async getEntities(prodId) {
+    return ProdAttr.query()
+      .where('product_id', prodId)
+      .fetch();
+  }
+
+  static async getEntity(attrId) {
+    return ProdAttr.findOrFail(attrId);
+  }
+
+  static async storeEntity(prodId, { value, attribute_id: attributeId }) {
+    const product = await Product.findOrFail(prodId);
+    const type = await product.type().first();
     const prodTypeAttr = await ProductTypeAttribute.query()
       .where('product_type_id', type.id)
       .where('attribute_id', attributeId)
-      .fetch();
+      .first();
 
     const prodAttr = new ProdAttr();
     prodAttr.value = value;
-    prodAttr.product_id = productId;
-    prodAttr.product_type_attribute_id = prodTypeAttr.rows[0].id;
-    await prodAttr.save();
+    prodAttr.product_id = Number(prodId);
+    prodAttr.product_type_attribute_id = prodTypeAttr.id;
+    prodAttr.save();
 
     return prodAttr;
   }
 
-  static async updateEntity(id, { value, product_id: productId, attribute_id: attributeId }) {
-    const product = await Product.findOrFail(productId);
-    const type = await product.type().fetch();
-    const prodTypeAttr = await ProductTypeAttribute.query()
-      .where('product_type_id', type.id)
-      .where('attribute_id', attributeId)
-      .fetch();
-
-    const prodAttr = await ProdAttr.findOrFail(id);
+  static async updateEntity(attrId, { value }) {
+    const prodAttr = await ProdAttr.findOrFail(attrId);
     prodAttr.value = value;
-    prodAttr.product_id = productId;
-    prodAttr.product_type_attribute_id = prodTypeAttr.rows[0].id;
-    await prodAttr.save();
-
+    prodAttr.save();
     return prodAttr;
   }
 
-  static async deleteEntity(id) {
-    const prodAttr = await ProdAttr.findOrFail(id);
+  static async deleteEntity(attrId) {
+    const prodAttr = await ProdAttr.findOrFail(attrId);
     await prodAttr.delete();
 
     return { result: `ProductAttribute(ID: ${prodAttr.id}) was deleted'` };
--- app/Models/Repositories/ProductTypeAttribute.js
+++ app/Models/Repositories/ProductTypeAttribute.js
@@ -1,26 +1,35 @@
 const ProdTypeAttr = use('App/Models/ProductTypeAttribute');
 
 class ProductTypeAttribute {
-  static async storeEntity({ product_type_id: prodTypeId, attribute_id: attributeId }) {
+  static async getEntities(typeId) {
+    return ProdTypeAttr.query()
+      .where('product_type_id', typeId)
+      .fetch();
+  }
+
+  static async getEntity(attrId) {
+    return ProdTypeAttr.findOrFail(attrId);
+  }
+
+  static async storeEntity(typeId, { attribute_id: attributeId }) {
     const prodTypeAttr = new ProdTypeAttr();
-    prodTypeAttr.product_type_id = prodTypeId;
-    prodTypeAttr.attribute_id = attributeId;
+    prodTypeAttr.product_type_id = Number(typeId);
+    prodTypeAttr.attribute_id = Number(attributeId);
     await prodTypeAttr.save();
 
     return prodTypeAttr;
   }
 
-  static async updateEntity(id, { product_type_id: prodTypeId, attribute_id: attributeId }) {
-    const prodTypeAttr = await ProdTypeAttr.findOrFail(id);
-    prodTypeAttr.product_type_id = prodTypeId;
-    prodTypeAttr.attribute_id = attributeId;
-    await prodTypeAttr.save();
+  static async updateEntity(attrId, { attribute_id: attributeId }) {
+    const prodTypeAttr = await ProductTypeAttribute.findOrFail(attrId);
+    prodTypeAttr.attribute_id = Number(attributeId);
+    prodTypeAttr.save();
 
     return prodTypeAttr;
   }
 
-  static async deleteEntity(id) {
-    const prodTypeAttr = await ProdTypeAttr.findOrFail(id);
+  static async deleteEntity(attrId) {
+    const prodTypeAttr = await ProdTypeAttr.findOrFail(attrId);
     await prodTypeAttr.delete();
 
     return { result: `ProductTypeAttribute(ID: ${prodTypeAttr.id}) was deleted'` };
