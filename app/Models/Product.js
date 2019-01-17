264a8ba3a8affb2403fa7f1ee1cd1f1e53e5384d
Bohdan Butko <bohdanbutko@gmail.com>
2019-01-17 13:28:23 +0200

    Add withPivot to Product relation

--- app/Models/Product.js
+++ app/Models/Product.js
@@ -14,7 +14,9 @@ class Product extends Entity {
   }
 
   attributes() {
-    return this.belongsToMany('App/Models/Attribute').pivotModel('App/Models/ProductAttribute');
+    return this.belongsToMany('App/Models/Attribute')
+      .pivotModel('App/Models/ProductAttribute')
+      .withPivot(['value']);
   }
 }
 
